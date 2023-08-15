9611faa47c9f8a7c52906c27db6634daf82752bf
Tom Fleet <tomfleet2018@gmail.com>
2023-08-15 08:16:32 +0100

    Expand test cases around config

--- config/config_test.go
+++ config/config_test.go
@@ -21,25 +21,68 @@ open = true
 bin = "code"
 name = "VSCode"
 `
+
+	minimal = `
+directory = "~/somewhere/else"
+`
+
+	bad = `some nonsense [(*&^)]`
 )
 
 func TestLoad(t *testing.T) {
-	in := strings.NewReader(good)
-	cfg, err := config.Load(in)
-
-	want := config.Config{
-		GitHub: config.GitHub{
-			Username: "FollowTheProcess",
-			Token:    "notatoken",
+	tests := []struct {
+		name    string
+		input   string
+		want    config.Config
+		wantErr bool
+	}{
+		{
+			name:  "happy path",
+			input: good,
+			want: config.Config{
+				GitHub: config.GitHub{
+					Username: "FollowTheProcess",
+					Token:    "notatoken",
+				},
+				Directory: "~/Development",
+				Editor: config.Editor{
+					Bin:  "code",
+					Name: "VSCode",
+					Open: true,
+				},
+			},
+			wantErr: false,
 		},
-		Directory: "~/Development",
-		Editor: config.Editor{
-			Bin:  "code",
-			Name: "VSCode",
-			Open: true,
+		{
+			name:    "minimal",
+			input:   minimal,
+			want:    config.Config{Directory: "~/somewhere/else"},
+			wantErr: false,
+		},
+		{
+			name:    "empty",
+			input:   "", // Only default should be Directory
+			want:    config.Config{Directory: "~/Development"},
+			wantErr: false,
+		},
+		{
+			name:    "syntax error",
+			input:   bad,
+			want:    config.Config{},
+			wantErr: true,
 		},
 	}
 
-	test.Ok(t, err)
-	test.Diff(t, cfg, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			in := strings.NewReader(tt.input)
+			got, err := config.Load(in)
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("err = %v, wantErr = %v", err, tt.wantErr)
+			}
+
+			test.Diff(t, got, tt.want)
+		})
+	}
 }
